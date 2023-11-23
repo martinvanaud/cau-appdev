4e693d1f1107b139aa242d46d7a4bb4963324bd1
Martin VANAUD <martin.vanaud@epitech.eu>
2023-11-23 13:16:51 +0900

    feat(medication provider) created a provider to access, read, and edit the medication read on the home page

---
+++ lib/providers/medication.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import 'package:medi_minder/enums/dosage.dart';
+import 'package:medi_minder/enums/medication.dart';
+
+import 'package:medi_minder/entity/dosage.dart';
+import 'package:medi_minder/entity/medication.dart';
+import 'package:medi_minder/entity/schedule.dart';
+
+class MedicationProvider with ChangeNotifier {
+  List<Medication> _medications = [];
+
+  MedicationProvider() {
+    initializeMedications();
+  }
+
+  List<Medication> get medications => _medications;
+
+  void initializeMedications() {
+    _medications = [
+      Medication(
+        type: MedicationType.pill,
+        name: 'Aspirin',
+        duration: 7,
+        dosages: [
+          Dosage(numberOfItems: 1, timeOfDay: Schedule(hour: 8, minute: 00) , timing: DosageTiming.afterMeal),
+        ],
+        notificationsEnabled: true,
+      ),
+      Medication(
+        type: MedicationType.cachet,
+        name: 'Comlivit',
+        duration: 7,
+        dosages: [
+          Dosage(numberOfItems: 1, timeOfDay: Schedule(hour: 8, minute: 00) , timing: DosageTiming.afterMeal),
+        ],
+        notificationsEnabled: true,
+      ),
+      Medication(
+        type: MedicationType.ampoule,
+        name: '5-HTP',
+        duration: 2,
+        dosages: [
+          Dosage(numberOfItems: 1, timeOfDay: Schedule(hour: 8, minute: 30) , timing: DosageTiming.whenever),
+        ],
+        notificationsEnabled: true,
+      ),
+    ];
+  }
+
+  void addMedication(Medication medication) {
+    _medications.add(medication);
+    notifyListeners();
+  }
+
+  void removeMedication(Medication medication) {
+    _medications.remove(medication);
+    notifyListeners();
+  }
+}
