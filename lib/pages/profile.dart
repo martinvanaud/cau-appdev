8625760cfdde931b2d45dd7b4a249fee426b150a
Clément FERNANDES <clement.fernandes@epitech.eu>
2023-11-30 10:47:58 +0900

    fix(profile page): fixed the problem with the 2 profile pages

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 
+import 'package:medi_minder/pages/changepassword.dart';
+
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
@@ -38,15 +40,70 @@ class _ProfilePageState extends State<ProfilePage> {
           }, icon: const Icon(Icons.logout))
         ],
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You email is', style: TextStyle(fontSize: 20),),
-            const SizedBox(
-              height: 20,
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: const Column(
+                children: [
+                  SizedBox(height: 30.0),
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage(''),
+                  ),
+                  SizedBox(height: 30.0),
+                  Text(
+                    'firebase username',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(loggedUser!.email!, style: TextStyle(fontSize: 20),),
+            const ExpansionTile(
+              title: Text('Profile informations',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                ),),
+              children: [
+                ListTile(
+                  title: Text('Name: ...'),
+                ),
+                ListTile(
+                  title: Text('Age: ...'),
+                ),
+                ListTile(
+                  title: Text('email: ...'),
+                ),
+              ],
+            ),
+            ExpansionTile(
+              title: const Text('Settings',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                ),),
+              children: [
+                ListTile(
+                  title: const Text('change password'),
+                  trailing: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ChangePasswordPage(),
+                            ));
+                      }, style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                      child: const Text("Change Password", style: TextStyle(color: Colors.black),)
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
