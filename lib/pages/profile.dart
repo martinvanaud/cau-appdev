bd9a7c8fec4b720c9a8bc8bcee3126439b043e91
dufaurethibault <thibault.dufaure-de-citres@epitech.eu>
2023-11-30 05:16:03 +0400

    refactor/change: change Profilepage to another file, adding DB link in profile page

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'ChangePasswordPage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -31,22 +31,81 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profile Page'),
+        title: const Text('Profile Page',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 30,
+          ),),
         actions: [
           IconButton(onPressed: (){
             FirebaseAuth.instance.signOut();
           }, icon: const Icon(Icons.logout))
         ],
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You email is', style: TextStyle(fontSize: 20),),
-            const SizedBox(
-              height: 20,
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: const Column(
+                children: [
+                  SizedBox(height: 30.0),
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage(''),
+                  ),
+                  SizedBox(height: 30.0),
+                  Text(
+                    'firebase username',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(loggedUser!.email!, style: TextStyle(fontSize: 20),),
+            ExpansionTile(
+              title: const Text('Profile informations',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                ),),
+              children: [
+                ListTile(
+                  title: Text('Name: ...'),
+                ),
+                ListTile(
+                  title: Text('Age: ...'),
+                ),
+                ListTile(
+                  title: Text("email: ${loggedUser!.email!}"),
+                ),
+              ],
+            ),
+            ExpansionTile(
+              title: const Text('Settings',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                ),),
+              children: [
+                ListTile(
+                  title: const Text('change password'),
+                  trailing: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ChangePasswordPage(loggedUser: loggedUser),
+                            ));
+                      }, style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                      child: const Text("Change Password", style: TextStyle(color: Colors.black),)
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
