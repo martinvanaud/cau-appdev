8c59e6044a9e6faf4d126a65ffd0b4dd5787c9f2
Martin VANAUD <martin.vanaud@epitech.eu>
2023-12-01 20:28:18 +0900

    added(file) ./pages/profile.dart

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:medi_minder/pages/changepassword.dart';
+
+import 'package:medi_minder/entity/profile.dart';
+
+import 'package:medi_minder/providers/profile.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -12,21 +17,12 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  final _authentication = FirebaseAuth.instance;
-  User? loggedUser;
+  late Stream<Profile?> getUserProfileFuture;
 
   @override
   void initState() {
     super.initState();
-    getCurrentUser();
-  }
-
-  void getCurrentUser() {
-    final user = _authentication.currentUser;
-
-    if (user != null) {
-      loggedUser = user;
-    }
+    getUserProfileFuture = getUserProfileStream();
   }
 
   @override
@@ -35,77 +31,178 @@ class _ProfilePageState extends State<ProfilePage> {
       appBar: AppBar(
         title: const Text('Profile Page'),
         actions: [
-          IconButton(onPressed: (){
-            FirebaseAuth.instance.signOut();
-          }, icon: const Icon(Icons.logout))
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: const Icon(Icons.logout),
+          ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: const Column(
-                children: [
-                  SizedBox(height: 30.0),
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundImage: AssetImage(''),
-                  ),
-                  SizedBox(height: 30.0),
-                  Text(
-                    'firebase username',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ],
+      body: StreamBuilder<Profile?>(
+        stream: getUserProfileStream(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            Profile? userProfile = snapshot.data;
+            return buildProfilePage(userProfile);
+          } else {
+            return const Center(child: Text('No user profile available'));
+          }
+        },
+      ),
+    );
+  }
+
+  Widget buildProfilePage(Profile? userProfile) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 30.0),
+                const CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage(''), // Update this accordingly
+                ),
+                const SizedBox(height: 30.0),
+                Text(
+                  userProfile?.username ?? 'Username not available',
+                  style: const TextStyle(fontSize: 20.0),
+                ),
+              ],
+            ),
+          ),
+          ExpansionTile(
+            title: const Text(
+              'Profile informations',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 30,
               ),
             ),
-            const ExpansionTile(
-              title: Text('Profile informations',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 30,
-                ),),
-              children: [
-                ListTile(
-                  title: Text('Name: ...'),
-                ),
-                ListTile(
-                  title: Text('Age: ...'),
-                ),
-                ListTile(
-                  title: Text('email: ...'),
-                ),
-              ],
+            children: [
+              EditableProfileField(
+                title: 'Username',
+                value: userProfile?.username ?? '',
+                onEditComplete: (newValue) => updateProfile('username', newValue),
+              ),
+              ListTile(
+                title: Text("Email: ${userProfile?.email}"),
+              ),
+              EditableProfileField(
+                title: 'Age',
+                value: userProfile?.age ?? '',
+                onEditComplete: (newValue) => updateProfile('age', newValue),
+              ),
+              EditableProfileField(
+                title: 'Height',
+                value: userProfile?.height ?? '',
+                onEditComplete: (newValue) => updateProfile('height', newValue),
+              ),
+              EditableProfileField(
+                title: 'Weight',
+                value: userProfile?.weight ?? '',
+                onEditComplete: (newValue) => updateProfile('weight', newValue),
+              ),
+            ],
+          ),
+          ExpansionTile(
+            title: const Text(
+              'Settings',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 30,
+              ),
             ),
-            ExpansionTile(
-              title: const Text('Settings',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 30,
-                ),),
-              children: [
-                ListTile(
-                  title: const Text('change password'),
-                  trailing: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ChangePasswordPage(),
-                            ));
-                      }, style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                  ),
-                      child: const Text("Change Password", style: TextStyle(color: Colors.black),)
+            children: [
+              ListTile(
+                title: const Text('change password'),
+                trailing: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ChangePasswordPage(),
+                          ));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                    ),
+                    child: const Text(
+                      "Change Password",
+                      style: TextStyle(color: Colors.black),
+                    )),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class EditableProfileField extends StatelessWidget {
+  final String title;
+  final String value;
+  final IconData icon;
+  final Function(String) onEditComplete;
+
+  const EditableProfileField({
+    Key? key,
+    required this.title,
+    required this.value,
+    this.icon = Icons.edit,
+    required this.onEditComplete,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text('$title: $value'),
+      trailing: IconButton(
+        icon: Icon(icon),
+        onPressed: () async {
+          final TextEditingController controller = TextEditingController(text: value);
+          await showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text('Edit $title'),
+                content: TextField(
+                  controller: controller,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your $title',
                   ),
                 ),
-              ],
-            ),
-          ],
-        ),
+                actions: [
+                  TextButton(
+                    child: const Text('Validate'),
+                    onPressed: () {
+                      if (controller.text.trim().isEmpty) {
+                        // If field is empty, show SnackBar
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('Please fill in the $title'),
+                            backgroundColor: Colors.red,
+                          ),
+                        );
+                      } else {
+                        Navigator.of(context).pop();
+                        onEditComplete(controller.text);
+                      }
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
       ),
     );
   }
