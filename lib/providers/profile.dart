bd2a8112a7778b7c61b4edc3b17ab68c5b67a129
Martin VANAUD <martin.vanaud@epitech.eu>
2023-12-01 20:28:18 +0900

    added(file) ./providers/profile.dart

---
+++ lib/providers/profile.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future<void> updateProfile(String field, String value) async {
+  User? user = FirebaseAuth.instance.currentUser;
+  String uid = user?.uid ?? '';
+
+  if (uid.isNotEmpty) {
+    try {
+      await FirebaseFirestore.instance.collection('users').doc(uid).update({
+        field: value,
+      });
+    } catch (e) {
+      print('Error updating profile: $e');
+      // Handle errors, e.g., show a snackbar with the error message
+    }
+  }
+}
