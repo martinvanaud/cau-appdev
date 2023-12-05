62685d5cca3b9593027b5a7868bce32ffad0a106
Clément FERNANDES <clement.fernandes@epitech.eu>
2023-12-05 10:19:24 +0900

    feat(login): added the login and register style

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -4,22 +4,27 @@ import 'package:firebase_auth/firebase_auth.dart';
 import '../main.dart';
 
 class RegisterPage extends StatelessWidget {
-  const RegisterPage({super.key});
+  const RegisterPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Register'),
+      appBar: AppBar(
+        title: const Text(
+          'Create Account',
+          style: TextStyle(
+            color: Color(0xFF1F41BB),
+            fontSize: 24.0,
+          ),
         ),
-        body: const RegisterForm()
+      ),
+      body: const RegisterForm(),
     );
   }
 }
 
-
 class RegisterForm extends StatefulWidget {
-  const RegisterForm({super.key});
+  const RegisterForm({Key? key});
 
   @override
   State<RegisterForm> createState() => _RegisterFormState();
@@ -30,69 +35,139 @@ class _RegisterFormState extends State<RegisterForm> {
   final _formKey = GlobalKey<FormState>();
   String email = '';
   String password = '';
+  String confirmPassword = '';
 
   @override
   Widget build(BuildContext context) {
-
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Form(
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Form(
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           key: _formKey,
           child: ListView(
             children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Email',
+              const Text(
+                "Create an account so you can use",
+                style: TextStyle(
+                  fontSize: 18.0,
                 ),
-                onChanged: (value) {
-                  email = value;
-                },
+                textAlign: TextAlign.center,
+              ),
+              const Text(
+                "MediMinders",
+                style: TextStyle(
+                  fontSize: 18.0,
+                ),
+                textAlign: TextAlign.center,
               ),
               const SizedBox(
-                height: 20,
+                height: 60,
               ),
-              TextFormField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xFFF1F4FF),
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
-                onChanged: (value) {
-                  password = value;
-                },
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 2,
+                        color: Color(0xFF1F41BB),
+                      ),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                ),
               ),
               const SizedBox(
-                height: 20,
+                height: 30,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xFFF1F4FF),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 2,
+                        color: Color(0xFF1F41BB),
+                      ),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  validator: (value) {
+                    if (value == null || value.length < 6) {
+                      return 'Password must be at least 6 characters';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 30,
               ),
               ElevatedButton(
-                  onPressed: () async {
-                    final newUser = await _authentication.createUserWithEmailAndPassword(
-                        email: email,
-                        password: password
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    final currentUser = await _authentication.createUserWithEmailAndPassword(
+                      email: email,
+                      password: password,
                     );
 
-                    if (newUser != null) {
+                    if (currentUser.user != null) {
                       _formKey.currentState!.reset();
                     }
 
                     if (!mounted) return;
                     Navigator.push(context, MaterialPageRoute(builder: (context) => const MyHomePage()));
-                  },
-                  child: const Text('Enter')
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF1F41BB),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  minimumSize: const Size(double.infinity, 60),
+                ),
+                child: const Text(
+                  'Sign up',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  const Text('If you already registered ,'),
                   TextButton(
-                      onPressed: (){
-                        Navigator.pop(context);
-                      },
-                      child: const Text('login with your email')
-                  )
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Already have an account', style: TextStyle(color: Color(0xFF1F41BB)),),
+                  ),
                 ],
               ),
             ],
-          )
+          ),
+        ),
       ),
     );
   }
